c07776156997e4713858872e65b12ccfac6ea620
AlefeCma <alefedonato777hotmail.com@gmail.com>
2022-09-15 21:10:10 -0300

    Classe de apoio estavel(Melhorar)

--- src/view/Apoio.java
+++ src/view/Apoio.java
@@ -4,10 +4,25 @@
  */
 package view;
 
-/**
- *
- * @author alefe
- */
+import DAO.UsuarioDAO;
+import DTO.Usuario;
+
 public class Apoio {
-    
+
+    public static void create() {
+        Tela_Cadastro tl = new Tela_Cadastro();
+        
+        String nome = tl.CampoNome.getText();
+        String email = tl.CampoEmail.getText();
+        String senha = tl.CampoSenha.getText();
+        
+        Usuario usuario = new Usuario();
+
+        usuario.setNome_usuario(nome);
+        usuario.setEmail_usuario(email);
+        usuario.setSenha_senha(senha);
+
+        UsuarioDAO DAO = new UsuarioDAO();
+        DAO.create(usuario);
+    }
 }
