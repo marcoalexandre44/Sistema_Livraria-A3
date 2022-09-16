ec7dd7b7fb91723f99914de90fbbc2fd2d1d8ddd
AlefeCma <alefedonato777hotmail.com@gmail.com>
2022-09-15 21:11:13 -0300

    Create para cadastrar usuarios criado

--- src/DAO/UsuarioDAO.java
+++ src/DAO/UsuarioDAO.java
@@ -3,11 +3,40 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package DAO;
+import DTO.Usuario;
+import conexao.Conexao;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author alefe
  */
 public class UsuarioDAO {
+    Connection cn;
+    PreparedStatement stm;
+    
+    public void create(Usuario user){
+        
+        cn = new Conexao().conectarBD();
+        try 
+        {
+            stm = cn.prepareStatement("INSERT INTO user (nome_usuario , email_usuario ,senha_usuario)VALUES(?,?,?)");
+            stm.setString(1, user.getNome_usuario());
+            stm.setString(2, user.getEmail_usuario());
+            stm.setString(3, user.getSenha_senha());
+            stm.executeUpdate();
+        } 
+        catch (SQLException ex)
+        {
+            System.out.println(" Erro ao cadrastar usuario " + ex);
+        }
+    }
+    
+    
     
 }
