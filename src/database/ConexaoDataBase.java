c3ba6d6f9dee714ff7a8ae77675306a73913ae42
AlefeCma <alefedonato777hotmail.com@gmail.com>
2022-09-26 13:54:17 -0300

    Mudança do nome da conexão

---
+++ src/database/ConexaoDataBase.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package database;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author alefe
+ */
+public class ConexaoDataBase {
+    Connection cn = null;
+    String url = "jdbc:mysql://localhost:3306/a3_Livraria?user=root&password=";
+    public Connection conectarBD(){
+    
+        try {
+            cn = DriverManager.getConnection(url);
+        } catch (SQLException ex) {
+            System.out.println("Erro ao conectar no Banco de dados" + ex);
+        } 
+        return cn;
+    } 
+}
