e7815b854b66c135dce809fa3396afcd14191e97
AlefeCma <alefedonato777hotmail.com@gmail.com>
2022-11-10 09:44:41 -0300

    Atualizações do projeto

--- test/controllers/UtilTest.java
+++ test/controllers/UtilTest.java
@@ -1,25 +1,24 @@
 package controllers;
 
+import DAO.LivroDAO;
 import DAO.UsuarioDAO;
 import DTO.Usuario;
 import static junit.framework.Assert.assertEquals;
 import org.junit.Assert;
 import org.junit.Test;
-
+import DTO.Livro;
 public class UtilTest {
 
-    // private util u;
+    private int id;
+    private String titulo;
+    private String categoria;
+    private String autor;
+    private String editora;
+    private   Livro livro = new Livro("joão e o pe de feijão","aventura","desconhecido","123");
+    
     @Test
     public void testeCriptografia() {
         String senha = "123";
         assertEquals("959e8cmla530336a9mm65db9447753c64bb73fbli5fdeil7hl47nlhb", Util.criptografarSenha(senha));
-    }
-
-    public void testeCreate() {
-        Usuario usuario = new Usuario("alefe gustavo", "alefe@gmail.com.br", "123");
-        UsuarioDAO dao = new UsuarioDAO();
-        dao.create(usuario);
-        // Assert.assertThrows();
-
     }
 }
