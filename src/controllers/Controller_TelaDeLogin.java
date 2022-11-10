9bad5f4b277514aeb492db49293c09fef195e8f2
AlefeCma <alefedonato777hotmail.com@gmail.com>
2022-11-10 09:45:17 -0300

    Atualizações do projeto

--- src/controllers/Controller_TelaDeLogin.java
+++ src/controllers/Controller_TelaDeLogin.java
@@ -4,35 +4,30 @@
  */
 package controllers;
 
-import controllers.Controller_TelaDeCadatro;
 import DAO.UsuarioDAO;
 import DTO.Usuario;
 import javax.swing.JOptionPane;
-import static controllers.Controller_TelaDeCadatro.create;
 import view.Tela_Admin;
 import view.Tela_UsuarioComun;
 
-/**
- *
- * @author alefe
- */
 public class Controller_TelaDeLogin {
-    
+
 //Verfica se existe o usuario no banco se sim ele chama o metodo validarNivelDeUsuario
-    public static void autentica_usuarioLogin(String email, String senha) {
+    public static boolean autentica_usuarioLogin(String email, String senha) {
         Usuario usuario = new Usuario();
         UsuarioDAO DAO = new UsuarioDAO();
         usuario.setEmail_usuario(email);
         usuario.setSenha_senha(Util.criptografarSenha(senha));
 
         if (DAO.autenticaUsuarioLogin(usuario) == true) {
-            validarNivelDeUsuario(email,Util.criptografarSenha(senha));
+            validarNivelDeUsuario(email, Util.criptografarSenha(senha));
+            return true;
         } else {
             JOptionPane.showMessageDialog(null, "E-mail ou senha incorretos ");
         }
+        return false;
     }
-    
-    
+
 // Valida o nivel do usuario se for admin abre se a tela de admin se for diferente de admin abra se usuario comum
     public static void validarNivelDeUsuario(String email, String senha) {
 
@@ -41,12 +36,11 @@ public class Controller_TelaDeLogin {
         usuario.setEmail_usuario(email);
         usuario.setSenha_senha(senha);
 
-        if (DAO.verificarAdmin(usuario) == true){
+        if (DAO.verificarAdmin(usuario) == true) {
             chamarTelaDeAdmin();
         } else {
             chamarTelaDeComun();
         }
-
     }
 
     public static void chamarTelaDeAdmin() {
